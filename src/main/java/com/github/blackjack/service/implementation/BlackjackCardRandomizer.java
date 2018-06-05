48411d17ce93c4bae8fa8bc6a0319419ec9b634b
mariusz <mariusz.maciula@gmail.com>
2018-06-05 05:14:35 +0200

    Changed extends in BlackjajckCardRandomizer

--- src/main/java/com/github/blackjack/service/implementation/BlackjackCardRandomizer.java
+++ src/main/java/com/github/blackjack/service/implementation/BlackjackCardRandomizer.java
@@ -1,12 +1,13 @@
 package com.github.blackjack.service.implementation;
 
+import com.github.blackjack.model.BlackjackCard;
 import com.github.blackjack.model.Card;
 import com.github.blackjack.service.Randomizer;
 
 import java.util.List;
 import java.util.Random;
 
-public class BlackjackCardRandomizer implements Randomizer {
+public class BlackjackCardRandomizer implements Randomizer<Card> {
     private static final Random RANDOM = new Random();
 
     @Override
